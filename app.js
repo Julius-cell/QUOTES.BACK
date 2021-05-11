d179caa706e35cac50e4242a54a33a7a2f593041
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-05-11 09:16:42 -0400

    Adding cookie parser

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +31,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 // Data sanitization NoSQL query injection
 app.use(mongoSanitize());
 // Data sanitization against XSS
@@ -46,6 +48,12 @@ app.use(express.urlencoded({ extended: true }));
 // -------------------  STATICS FILES  -------------------
 app.use(express.static('public'));
 // -------------------------------------------------------
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
+  next();
+})
 
 // -------------------  ROUTES  --------------------------
 app.use('/api/quotes/', quoteRouter);
