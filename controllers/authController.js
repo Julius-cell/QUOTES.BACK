a2b1d6f7fcb5bfc845f6a6538e6182ed55dcd612
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-05-13 21:41:27 -0400

    isLoggendIn added in auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -117,6 +117,35 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 
+// Only for rendered pages, no errors!
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) Verify Token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+      // THERE IS A LOGGED USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (error) {
+      return next();
+    }
+  }
+  next();
+}
+
+
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     // roles is an array ['admin'] example
